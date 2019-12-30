36f50bb0ba02842a5554314653b93228c5eaf341
Jason Tame <jason@pango.studio>
2019-12-30 19:49:58 +0200

    Add background colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    colors: {
+      darkslategrey: '#354c3a',
+      peru: '#9d8c44',
+      saddlebrown: '#6a1d2f',
+      sandybrown: '#dea450',
+      sienna: '#9b461f'
+    }
+  },
   variants: {},
   plugins: []
 }
